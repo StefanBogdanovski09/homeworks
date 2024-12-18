63954cfca26abecef875f13b7b73ccb98a109f36
StefanBogdanovski09 <stefan_kobra@live.com>
2024-12-18 04:10:48 +0100

    homework
    homework

---
+++ Homework/code/script.js
@@ -0,0 +1,34 @@
+let onePhoneInDollars = 119.95;
+let phones = 30;
+let taxesPercentage = 5;
+let priceNoTax = onePhoneInDollars * phones;
+let tax = (priceNoTax * taxesPercentage) / 100;
+let totalPrice = priceNoTax + tax;
+console.log(totalPrice);
+
+
+let minutes = 10;
+let oneSecond = 60;
+let result = minutes * oneSecond;
+console.log(result);
+
+let number = 23;
+let result1 = number + 1;
+console.log(result1);
+
+
+let base = 4;
+let height = 6;
+let area = (base * height) / 2;
+console.log(area);
+
+
+let lenght = 4;
+let witdh = 5;
+let perimetar = 2 * (lenght + witdh);
+console.log(perimetar);
+
+let radius = 9;
+let value = Math.PI;
+let areOfCircle = value * radius * radius;
+console.log(areOfCircle);
